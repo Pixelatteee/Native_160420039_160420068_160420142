911cf0c73e08fd68c017d2068aca4f3e7db505e2
Pixelatteee <thewarrior312@gmail.com>
2023-03-01 14:36:51 +0700

    test

---
+++ app/src/main/java/com/ubaya/native_160420039_160420068_160420142/Testing.kt
@@ -0,0 +1,11 @@
+package com.ubaya.native_160420039_160420068_160420142
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Testing : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_testing)
+    }
+}
